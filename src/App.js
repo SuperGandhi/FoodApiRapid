373abd81a0d3da99ab091d68c34f0c8ba65ffaa3
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-06 16:57:27 -0500

    adding hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+// import axios from "axios";
+
+
 
 
 function App() {
+  const [endPoint, setEndPoints]= useState('')
+  const [container, setContainer] = useState([])
 
+  useEffect(() => {
+    fethMe()
+  },[endPoint])
+
+  const fethMe = () =>{
   const options = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com',
-      'X-RapidAPI-Key': 'd8818fa09emshcd4bed22312084fp14331djsn23e53f613ef5'
+      'X-RapidAPI-Key': '490574ce41mshbca8c481c71683bp1f17e6jsn17e91bcc55ac'
     }
   };
   
-  fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
+  fetch(`https://online-movie-database.p.rapidapi.com/auto-complete?q+${endPoint}`, options)
     .then(response => response.json())
     .then(response => console.log(response))
+    .then(data => {
+      setContainer(data)
+    })
     .catch(err => console.error(err));
-    
+}
+
+  const onChangeHandler = (e)=>{
+    setEndPoints(e.target.value)
+  }
+
+  const submitHandler = (e) =>{
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
-
+      <form onSubmit={submitHandler}>
+        <input type="text" value={endPoint} onChange={onChangeHandler}/>
+        <button type="submit">submit</button>
+      </form>
+      {container.map((item) => {
+        return (
+          <div>
+            <img src={item.i.imageUrl} alt="" />
+            <p>{item.l}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
 
