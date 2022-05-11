fbfd8bad0201dadc9d8229b8ffce21e478072901
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-11 12:44:45 -0500

    adding functionality to useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
-// import axios from "axios";
+
 
 
 
@@ -11,33 +11,52 @@ function App() {
   
   const [query, setQuery] = useState('')
   const [container,setContainer] = useState([])
-  
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com',
-      'X-RapidAPI-Key': 'd8818fa09emshcd4bed22312084fp14331djsn23e53f613ef5'
-    }
-  };
-  
-  fetch('https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=apple', options)
-    .then(response => {
-      return response.json();
-    })
-    .then(data =>{
-      setContainer(data.hints)
-    })
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+  const [endPoint, setEndPoint] = useState('')
 
+  useEffect(()=> {
+    const options = {
+      // 'method': 'GET',
+      // headers: {
+        //   'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com',
+        //   'X-RapidAPI-Key': 'd8818fa09emshcd4bed22312084fp14331djsn23e53f613ef5'
+        // }
+      };
+      
+      fetch(`https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr= + ${query}`, options)
+      .then(response => {
+        return response.json();
+      })
+      .then(data =>{
+        setContainer(data.hints)
+      })
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+},[endPoint])
+      
+      const onChangeHandler = (e) =>{
+        setQuery(e.target.value)
+      }
 
-  
+      const onSubmitHandler = e =>{
+        e.preventDefault()
+        setEndPoint(query)
+      }
+      
+      
   return(
     <div>
-       <form action="">
-         <input type="text" />
+       <form onSubmit={onSubmitHandler}>
+         <input type="text" value={query} onChange={onChangeHandler}/>
          <button type='submit'>Submit</button>
        </form>
+       {container.map((item)=> {
+         return (
+           <div>
+             <img src={item.food.image} alt="" />
+             <p>{item.food.label}</p>
+           </div>
+         ) 
+       })}
     </div>
   );
 }
